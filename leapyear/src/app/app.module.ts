bb85dfadaa275a81f5bc68c6458ca86da76c034c
BSB <hello@ballaschottner.com>
2022-09-12 22:37:16 +0200

    about this project

--- leapyear/src/app/app.module.ts
+++ leapyear/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,6 +22,7 @@ import { AboutMeComponent } from './components/about-me/about-me.component';
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
